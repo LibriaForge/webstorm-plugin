1041e2a16c606558808e8b43052b1b21cc4aa192
GitHub Action <action@github.com>
2026-02-02 11:36:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/pfms84/webstormplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/pfms84/webstormplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.pfms84.webstormplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
